438480d6c0f058803b44e5e3edb05a286155a870
FreakieGamer101 <AWhitehouse@student.neumont.edu>
2022-02-28 11:55:04 -0700

    Update Goal.cs

--- Assets/Scripts/Minigame_Goal!/Goal.cs
+++ Assets/Scripts/Minigame_Goal!/Goal.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 public class Goal : MonoBehaviour
 {
     public GameObject[] spaces = new GameObject[99];
+    public Image end, movable;
     private List<List<GameObject>> map = new List<List<GameObject>>();
+    private GameObject goal, player;
+    private int col = 0, row = 0, endCol = 0, endRow = 0;
+    private List<string> moves = new List<string>();
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +34,9 @@ public class Goal : MonoBehaviour
             {
                 first++;
             }
-        }        
+        }
+
+        RandomSpawn();
     }
 
     // Update is called once per frame
@@ -39,16 +47,49 @@ public class Goal : MonoBehaviour
 
     void Move()
     {
-
+        moves.Add("Move");
     }
 
     void Left()
     {
-
+        moves.Add("Left");
     }
 
     void Right()
     {
+        moves.Add("Right");
+    }
 
+    void Undo()
+    {
+        if(moves.Count > 0)
+        {
+            moves.RemoveAt(moves.Count - 1);
+        }
+    }
+
+    void Run()
+    {
+
+    }
+
+    void RandomSpawn()
+    {
+        var rand = new System.Random();
+        col = rand.Next(0, map.Count);
+        row = rand.Next(0, map[col].Count);
+
+        player = map[col][row];
+
+        endCol = rand.Next(0, map.Count);
+        endRow = rand.Next(0, map[col].Count);
+
+        while(col - endCol > 2 && row - endRow > 2)
+        {
+            endCol = rand.Next(0, map.Count);
+            endRow = rand.Next(0, map[endCol].Count);
+        }
+
+        goal = map[endCol][endRow];
     }
 }
