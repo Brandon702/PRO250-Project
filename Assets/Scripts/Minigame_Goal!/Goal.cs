511c90fce6f93edd5736022964bdac68469a15ec
FreakieGamer101 <AWhitehouse@student.neumont.edu>
2022-03-01 09:58:40 -0700

    Updated Goal Script

--- Assets/Scripts/Minigame_Goal!/Goal.cs
+++ Assets/Scripts/Minigame_Goal!/Goal.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 
 public class Goal : MonoBehaviour
 {
-    public GameObject[] spaces = new GameObject[99];
-    public Image end, movable;
-    private List<List<GameObject>> map = new List<List<GameObject>>();
-    private GameObject goal, player;
-    private int col = 0, row = 0, endCol = 0, endRow = 0;
+    public Image[] spaces = new Image[99];
+    public GameObject emptySpace;
+    public Sprite end, movable;
+    private List<List<Image>> map = new List<List<Image>>();
+    private Image goal, player;
+    private int col = 0, row = 0, endCol = 0, endRow = 0, mapCol, mapRow;
     private List<string> moves = new List<string>();
 
     // Start is called before the first frame update
@@ -18,11 +19,11 @@ public class Goal : MonoBehaviour
     {
         int first = 0;
         int second = 0;
-        foreach(GameObject space in spaces)
+        foreach(Image space in spaces)
         {
             if(map.Count == second)
             {
-                map.Add(new List<GameObject>());
+                map.Add(new List<Image>());
             }
             map[second].Add(space);
             if(first >= 10)
@@ -79,7 +80,10 @@ public class Goal : MonoBehaviour
         col = rand.Next(0, map.Count);
         row = rand.Next(0, map[col].Count);
 
-        player = map[col][row];
+        mapCol = col;
+        mapRow = row;
+
+        map[col][row] = player;
 
         endCol = rand.Next(0, map.Count);
         endRow = rand.Next(0, map[col].Count);
@@ -90,6 +94,6 @@ public class Goal : MonoBehaviour
             endRow = rand.Next(0, map[endCol].Count);
         }
 
-        goal = map[endCol][endRow];
+        map[endCol][endRow] = goal;
     }
 }
